97c8deccd4254139c603af21dbedc62fa0055f17
Maia <raafamaia@gmail.com>
2015-01-29 14:30:54 -0200

    total stats

--- models/Character.js
+++ models/Character.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var Equipment = require('./Equipment');
+
 function Character(name, hp, weapon, shield, strength, defense){
 	this.name = name;
 	this.hp = hp;
@@ -7,6 +9,8 @@ function Character(name, hp, weapon, shield, strength, defense){
 	this.shield = shield;
 	this.strength = (strength === undefined) ? 10 : strength;
 	this.defense = (defense === undefined) ? 10 : defense;
+	this.totalStrength = this.weapon.attack + this.strength;
+	this.totalDefense = this.shield.defense + this.defense;
 	this.alive = true;
 }
 
@@ -20,11 +24,7 @@ Character.prototype = {
 
 	attack: function(target){
 		if(target instanceof Character){
-			var dmg = (this.strength
-			+ ((this.weapon === undefined) ? 0 : this.weapon.attack))
-			- (target.defense
-			+ ((target.shield === undefined) ? 0 : target.shield.defense));
-
+			var dmg = this.totalStrength - this.totalDefense;
 			target.lostHP(dmg);
 			if(target.checkDead()){
 				target.alive = false;
@@ -37,13 +37,12 @@ Character.prototype = {
 	toString: function(){
 		var message = "Hey, I'm the brave " + this.name + "\nYou better fear me fool...\n"
 		+ "Why? Huh, just take a look:\n";
-
-		for (property in this){
+		for(var property in this){
 			if (this.hasOwnProperty(property)){
 				message += "\n" + property + ": ";
 				if (this[property] instanceof Equipment){
-					for(prop in this[property]){
-						message += prop + " - " + this[property][prop] + "\n\t\t\t\t";
+					for(var prop in this[property]){
+						message += prop + " - " + this[property][prop] + "\n\t";
 					}
 				} else {
 					message += this[property];
