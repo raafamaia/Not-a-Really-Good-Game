367166b0f1952c1b8ecb72293e2b6d2316d02fbb
Maia <raafamaia@gmail.com>
2015-02-03 15:07:01 -0200

    lucky game

--- models/Ogre.js
+++ models/Ogre.js
@@ -5,8 +5,18 @@ var Character = require('./Character');
 
 function Ogre(){
 	Character.apply(this, arguments);
+	this.arr = [];
 }
 Ogre.prototype = subclassOf(Character);
+Ogre.prototype.challenge = function(){
+	this.arr = [];
+	var i = 0;
+	while(i < 3){
+		var n = Math.floor((Math.random() * 10) + 1);
+		this.arr.push((n % 2 === 0));
+		i++;
+	}
+};
 
 module.exports = Ogre;
 
